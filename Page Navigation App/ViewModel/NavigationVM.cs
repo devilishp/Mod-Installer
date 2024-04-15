4d67bee61eed1ae8f0b8f8f1bd961df57c770aec
Egor <kazakevich.ev52@gmail.com>
2024-04-15 10:16:43 +0300

    Добавьте файлы проекта.

---
+++ Page Navigation App/ViewModel/NavigationVM.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ModInstaller.Utilities;
+using System.Windows.Input;
+
+namespace ModInstaller.ViewModel
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand HomeCommand { get; set; }
+        public ICommand ProductsCommand { get; set; }
+        public ICommand SettingsCommand { get; set; }
+
+        private void Home(object obj) => CurrentView = new HomeVM();
+        private void Product(object obj) => CurrentView = new ProductVM();
+        private void Setting(object obj) => CurrentView = new SettingVM();
+
+        public NavigationVM()
+        {
+            HomeCommand = new RelayCommand(Home);
+            ProductsCommand = new RelayCommand(Product);
+            SettingsCommand = new RelayCommand(Setting);
+
+            // Startup Page
+            CurrentView = new HomeVM();
+        }
+    }
+}
